eda6ae76babaf4304649c0560e448cbf91c09392
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 08:42:10 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,12 +2,11 @@ set_project("unknown")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
--- Xmake maga húzza le a csomagokat:
 add_requires("qt 6.9.2", {configs = {widgets = true}})
 add_requires("nlohmann_json")
 
 target("unknown")
-    add_rules("qt.widgetapp") -- moc/uic/qrc automata
+    add_rules("qt.widgetapp")
     set_kind("binary")
     add_files("src/**.cpp", "ui/**.ui", "resources/**.qrc")
     add_headerfiles("include/**.h")
