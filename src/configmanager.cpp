03169fdf11ee1c688eaed0db136f3a04413750f0
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 06:45:44 +0200

    Update configmanager.cpp

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -1,1 +1,23 @@
+#include "configmanager.h"
+#include <QJsonDocument>
 
+void ConfigManager::saveConfig(const QString& telegramToken, const QString& chatId) {
+    QJsonObject config;
+    config["telegram_token"] = telegramToken;
+    config["chat_id"] = chatId;
+    QFile file("config.json");
+    if (file.open(QIODevice::WriteOnly)) {
+        file.write(QJsonDocument(config).toJson());
+        file.close();
+    }
+}
+
+void ConfigManager::loadConfig(QString& telegramToken, QString& chatId) {
+    QFile file("config.json");
+    if (file.open(QIODevice::ReadOnly)) {
+        QJsonObject config = QJsonDocument::fromJson(file.readAll()).object();
+        telegramToken = config["telegram_token"].toString();
+        chatId = config["chat_id"].toString();
+        file.close();
+    }
+}
