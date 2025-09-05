6feff98a5877177972e38dd6a08200371fdf238c
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 08:56:17 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,17 +2,18 @@ set_project("unknown")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
--- SYSTEM Qt (a gépeden lévő, nem letöltött)
-add_requires("qt", {system = true, configs = {qt = "6", widgets = true}})
-add_requires("nlohmann_json", {optional = true})
-
 target("unknown")
+    -- Qt integráció (moc/uic/rcc) a beállított Qt SDK-val
     add_rules("qt.widgetapp")
     set_kind("binary")
+
     add_files("src/**.cpp")
     add_headerfiles("include/**.h")
     add_includedirs("include")
-    if os.isdir("ui") then add_files("ui/**.ui") end
-    if os.isdir("resources") then add_files("resources/**.qrc") end
-    add_packages("qt")
-    if has_package("nlohmann_json") then add_packages("nlohmann_json") end
+
+    if os.isdir("ui") then
+        add_files("ui/**.ui")
+    end
+    if os.isdir("resources") then
+        add_files("resources/**.qrc")
+    end
