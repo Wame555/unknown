f8b6fe9b231eb43322f97416ee532a5d290a54e3
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 08:36:41 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,35 +1,15 @@
--- Projekt + nyelv
 set_project("unknown")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
--- --- Opcionális: Qt6 támogatás (ha van GUI, moc/uic/rcc kell) ---
--- Vedd ki a kommentet, ha Qt6-ot használsz.
-add_requires("qt", {configs = {qt = "6", widgets = true}})
-
--- --- Közkedvelt, fejfájásmentes JSON lib (header-only) ---
--- Ha a kód nlohmann_json-t használja:
+-- Xmake maga húzza le a csomagokat:
+add_requires("qt 6.9.2", {configs = {widgets = true}})
 add_requires("nlohmann_json")
 
 target("unknown")
-    -- Ha Qt-ot használsz, cseréld erre:
-    add_rules("qt.widgetapp")
-
+    add_rules("qt.widgetapp") -- moc/uic/qrc automata
     set_kind("binary")
-    add_files("src/**.cpp")
+    add_files("src/**.cpp", "ui/**.ui", "resources/**.qrc")
     add_headerfiles("include/**.h")
     add_includedirs("include")
-
-    -- Ha Qt kell:
-     add_packages("qt")
-
-    -- Ha nlohmann_json kell:
-     add_packages("nlohmann_json")
-
-    -- Windows GUI app esetén (ne nyisson konzolt):
-     if is_plat("windows") then
-        add_ldflags("/SUBSYSTEM:WINDOWS")
-     end
-
-    -- Ha vannak .ui vagy .qrc fájlok:
-    add_files("ui/**.ui", "resources/**.qrc")
+    add_packages("qt", "nlohmann_json")
