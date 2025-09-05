626afe586530b0c0c791a20c56acd993cd9df791
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 09:22:23 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,18 @@
--- xmake.lua (javított)
 set_project("unknown")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
--- IXWebSocket TLS: mbedTLS (nem OpenSSL, így nincs perl/windres szívás)
+-- IXWebSocket TLS mbedTLS-szel (nincs OpenSSL-szívás)
 add_requires("ixwebsocket", {configs = {ssl = "mbedtls"}})
 
--- cURL: Windows-on Schannel (nincs OpenSSL build)
-add_requires("libcurl", {configs = {with_ssl = "schannel"}})
+-- cURL Windows-on Schannel-lel (ha használod Telegramhoz)
+add_requires("libcurl", {configs = {with_ssl = "schannel"}, optional = true})
 
--- Opcionális: ha tényleg használod
+-- JSON, ha kell a kódban
 add_requires("nlohmann_json", {optional = true})
 
 target("unknown")
-    -- Qt a rendszerből jön (QTDIR), nem csomag
+    -- Qt a gépedről (QTDIR), nem csomagból
     add_rules("qt.widgetapp")
     set_kind("binary")
 
@@ -21,14 +20,9 @@ target("unknown")
     add_headerfiles("include/**.h")
     add_includedirs("include")
 
-    if os.isdir("ui") then
-        add_files("ui/**.ui")
-    end
-    if os.isdir("resources") then
-        add_files("resources/**.qrc")
-    end
+    if os.isdir("ui") then add_files("ui/**.ui") end
+    if os.isdir("resources") then add_files("resources/**.qrc") end
 
-    add_packages("ixwebsocket", "libcurl")
-    if has_package("nlohmann_json") then
-        add_packages("nlohmann_json")
-    end
+    add_packages("ixwebsocket")
+    if has_package("libcurl") then add_packages("libcurl") end
+    if has_package("nlohmann_json") then add_packages("nlohmann_json") end
