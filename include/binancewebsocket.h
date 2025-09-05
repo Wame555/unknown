d241d44ca23d6463c725be9d7918e068cd21c0b0
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 06:44:30 +0200

    Update binancewebsocket.h

--- include/binancewebsocket.h
+++ include/binancewebsocket.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <QObject>
+#include <ixwebsocket/IXWebSocket.h>
+
+class BinanceWebsocket : public QObject {
+    Q_OBJECT
+public:
+    explicit BinanceWebsocket(QObject* parent = nullptr);
+    void connectToBinance(const QStringList& symbols);
+
+signals:
+    void priceUpdated(const QString& symbol, double price);
+
+private:
+    ix::WebSocket webSocket;
+};
 
