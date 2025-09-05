e10a40c17280fc3d659b64c321d74d8248eed050
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 06:45:04 +0200

    Update telegramnotifier.h

--- include/telegramnotifier.h
+++ include/telegramnotifier.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <QObject>
+#include <QString>
 
+class TelegramNotifier : public QObject {
+    Q_OBJECT
+public:
+    TelegramNotifier(const QString& token, const QString& chatId, QObject* parent = nullptr);
+    void sendMessage(const QString& message);
+
+private:
+    QString token;
+    QString chatId;
+};
