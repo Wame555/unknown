78f287673ed6a809b50879bf14e36671e4f0097e
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 06:45:26 +0200

    Update configmanager.h

--- include/configmanager.h
+++ include/configmanager.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <QObject>
+#include <QString>
+#include <QJsonObject>
+#include <QFile>
+
+class ConfigManager : public QObject {
+    Q_OBJECT
+public:
+    void saveConfig(const QString& telegramToken, const QString& chatId);
+    void loadConfig(QString& telegramToken, QString& chatId);
+};
 
