b8d93749a8104dec54328172793de4d3371bb877
Wame555 <76699366+Wame555@users.noreply.github.com>
2025-09-05 06:44:00 +0200

    Update demoaccount.h

--- include/demoaccount.h
+++ include/demoaccount.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <QObject>
+#include <QMap>
+#include <QString>
 
+struct Position {
+    double entryPrice;
+    double amount;
+    double tpPrice;
+    double slPrice;
+};
+
+class DemoAccount : public QObject {
+    Q_OBJECT
+public:
+    explicit DemoAccount(QObject* parent = nullptr);
+    void addPosition(const QString& symbol, double entryPrice, double amount, double tpPrice, double slPrice);
+    void closePosition(const QString& symbol);
+    double calculatePnL(const QString& symbol, double currentPrice);
+    QMap<QString, Position> getPositions() const { return positions; }
+
+signals:
+    void pnlUpdated(const QString& symbol, double pnl);
+    void tpReached(const QString& symbol);
+    void slReached(const QString& symbol);
+    void positionClosed(const QString& symbol);
+
+private:
+    QMap<QString, Position> positions;
+};
